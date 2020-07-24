f4728836c4775ea0b72d782e8cf02acf27367cc9
Adarsh Kunwar <adarsh.infinite@gmail.com>
2020-07-25 01:30:54 +0530

    Create Normal.cpp

---
+++ Normal.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+auto speedup = [](){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	#ifndef ONLINE_JUDGE
+		freopen("alchemy_validation_input.txt", "r", stdin);
+		freopen("output2.txt", "w", stdout);
+	#endif
+	return 0;
+}();
+
+int main()
+{
+	int test;
+	cin>>test;
+	for(int t=1;t<=test;++t)
+	{
+		
+	}
+	return 0;
+}
